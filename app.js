d9b1cdfc6b264a5eb75d05614a58460cf16dc7c0
Souvikarjun <68196981+Souvikarjun@users.noreply.github.com>
2024-08-04 14:32:35 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,76 @@
+let container= document.querySelectorAll(".container");
+let boxes= document.querySelectorAll(".box");
+let reset= document.querySelectorAll("#reset");
+let newGameBtn= document.querySelectorAll("#newbtn");
+const msg_container = document.getElementsByClassName("msg-container");
+let msg=document.querySelectorAll("#msg");
+
+let turn0 =true;
+const winPatterns=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+];
+
+const resetGame =() => {
+    turn0 =true;
+    enableBoxes();
+    msg_container.id = 'hide';
+}
+
+
+boxes.forEach((box) =>{
+    box.addEventListener("click",() =>{
+        
+        if(turn0){
+            box.innerText="O";
+            turn0 = false;
+        } else {
+            box.innerText="X";
+            turn0 = true;
+        }
+        box.disabled = true;
+         checkWinner()
+    });
+});
+const disableBoxes = () =>{
+    for (let box of boxes) {
+        box.disabled = true;
+    }
+};
+
+const enableBoxes = () =>{
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText="";
+    }
+};
+
+
+const showWinner=(winner) =>{
+    msg_container.id.remove("hide");
+    msg.innerText="CONGRATULATIONS,winner is ${winner}";
+}
+
+const checkWinner = () => {
+    for (let pattern of winPatterns){
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+        
+    if( pos1Val != "" && pos2Val != "" && pos3Val != "" ){
+        if ( pos1Val=== pos2Val && pos2Val===pos3Val){
+          
+          showWinner(pos1Val);
+        }
+      }
+    }
+};
+
+newGameBtn.addEventListener("onClick",resetGame);
+reset.addEventListener("onClick",resetGame);
